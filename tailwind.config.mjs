0bbb4ec80a98782c87bc457d2b103c31b67dae5f
Sinskiy <sinskiy@proton.me>
2024-02-01 22:10:59 +0300

    feat: move to astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{astro,html,md,mdx,ts,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "white",
+      primary: "var(--primary)",
+      onSurface: "var(--on-surface)",
+      surface: {
+        DEFAULT: "var(--surface)",
+        low: "var(--surface--low)",
+      },
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
