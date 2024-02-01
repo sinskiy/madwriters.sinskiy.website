0c6959fc205ae764f0aeb3750f88996a8ae1f25e
Sinskiy <sinskiy@proton.me>
2024-02-01 21:21:04 +0300

    fix head + add tailwind

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = "A blog by Sinskiy";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Here I talk about my life and web development";
