8be078028791944da9f080a246138d44f48d8d71
Sinskiy <sinskiy@proton.me>
2024-02-03 19:46:52 +0300

    normalize search focus colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,12 @@ export default {
     colors: {
       transparent: "transparent",
       current: "currentColor",
+      normal: "hsl(var(--normal) / <alpha-value>)",
       inverse: "hsl(var(--inverse) / <alpha-value>)",
       primary: "var(--primary)",
       onSurface: "var(--on-surface)",
       surface: {
+        highest: "var(--surface--highest)",
         DEFAULT: "var(--surface)",
         low: "var(--surface--low)",
       },
