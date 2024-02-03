9eeebece60b15dc0c8bfdb04e93cfc8d1f022580
Sinskiy <sinskiy@proton.me>
2024-02-03 16:34:32 +0300

    normalize search svg color in light mode and change theme icon on theme change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,md,mdx,ts,tsx}"],
   theme: {
     colors: {
       transparent: "transparent",
       current: "currentColor",
-      white: "white",
+      inverse: "hsl(var(--inverse) / <alpha-value>)",
       primary: "var(--primary)",
       onSurface: "var(--on-surface)",
       surface: {
