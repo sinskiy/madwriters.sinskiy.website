a42392a3894907fbbdcee3eb31741792ec108df5
Sinskiy <sinskiy@proton.me>
2024-02-24 19:44:54 +0300

    move posts to its own component

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,6 @@
+import { getCollection } from "astro:content";
+import type { CollectionEntry } from "astro:content";
+
 export const SITE_TITLE = "Записки сумасшедшей & синского";
 export const SITE_DESCRIPTION = "Название говорит само за себя";
 export const WEB_TAGS_LIST = ["http", "html", "css", "js", "react", "design"];
@@ -24,3 +27,33 @@ export const links = [
     height: "22",
   },
 ];
+const filterPosts = (
+  isAuthor: boolean,
+  authorOrTag: string,
+  posts: CollectionEntry<"blog">[]
+) => {
+  if (isAuthor) {
+    const author = authorOrTag;
+    const filteredPosts = posts.filter((post: any) => {
+      return post.data.author === author;
+    });
+    return filteredPosts;
+  } else {
+    const tag = authorOrTag;
+    const filteredPosts = posts.filter((post: any) => {
+      return post.data.tags.includes(tag);
+    });
+    return filteredPosts;
+  }
+};
+const sortPosts = (posts: CollectionEntry<"blog">[]) => {
+  return posts.sort(
+    (a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf()
+  );
+};
+export const getPosts = async (isAuthor: boolean, authorOrTag: string) => {
+  const posts = await getCollection("blog");
+  const filteredPosts = filterPosts(isAuthor, authorOrTag, posts);
+  const sortedPosts = sortPosts(filteredPosts);
+  return sortedPosts;
+};
