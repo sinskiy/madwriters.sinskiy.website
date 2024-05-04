1b8f8bfb372111a05b7f8fac829526e8f9d4fccd
Sinskiy <sinskiy@proton.me>
2024-05-04 23:18:29 +0300

    seems like noone puts blog post content in rss, so remove it

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,6 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
-import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
-const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("blog");
@@ -13,9 +10,6 @@ export async function GET(context) {
     site: context.site,
     items: posts.map((post) => ({
       link: `/${post.data.author}/${post.slug}/`,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
-      }),
       ...post.data,
     })),
   });
