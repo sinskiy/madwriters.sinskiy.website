0130496927eba0599575079c516cdb10000e85cd
Sinskiy <sinskiy@proton.me>
2024-05-04 17:21:20 +0300

    add content to rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,9 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("blog");
@@ -9,12 +12,11 @@ export async function GET(context) {
     description: SITE_DESCRIPTION,
     site: context.site,
     items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      categories: post.data.categories,
-      customData: post.data.customData,
       link: `/${post.data.author}/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
+      ...post.data,
     })),
     stylesheet: "/pretty-feed-v3.xsl",
   });
