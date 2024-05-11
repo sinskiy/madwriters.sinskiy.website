f82bab044fa87f70862a327e987198bcd53d44a9
sinskiy <sinskiy@proton.me>
2024-05-11 11:33:01 +0300

    move breadcrumbs functions to paths.ts

---
+++ src/paths.ts
@@ -0,0 +1,39 @@
+export interface Path {
+  name: string;
+  URL: string;
+}
+
+export function filterAndFormatPaths(paths: string[]): Path[] {
+  const formattedPaths = formatPaths(paths);
+  const filteredPaths = formattedPaths.filter((path): path is Path => !!path);
+  return filteredPaths;
+}
+
+export function formatPaths(paths: string[]): Array<Path | undefined> {
+  let walked: string = "/";
+  const formattedPaths = paths.map((path, i) => {
+    if (pathIsRoot(path, i)) {
+      return { name: "home", URL: path };
+    } else if (!pathIsRoot(path, i) && pathIsEmpty(path)) {
+      return;
+    } else {
+      walked += path + "/";
+      path = walkedTooLong(walked) ? "..." : path;
+      return { name: path, URL: walked };
+    }
+  });
+  return formattedPaths;
+}
+
+export function pathIsRoot(path: string, i: number): boolean {
+  return i === 0 && path === "";
+}
+
+export function pathIsEmpty(path: string): boolean {
+  return path === "";
+}
+
+export function walkedTooLong(walked: string): boolean {
+  const MAX = 22;
+  return walked.length > MAX;
+}
