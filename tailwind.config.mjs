b8868a900441d0746896123bbb3dd32fa38b971a
sinskiy <sinskiy@proton.me>
2024-12-09 18:16:59 +0300

    feat: upgrade and remove ViewTransition

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import typography from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
@@ -22,5 +24,5 @@ export default {
     },
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [typography],
 };
